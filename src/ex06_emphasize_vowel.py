bda8766c459ce419a75599a3786e6cb53874b4db
Csalinaron <csalinaron@educacion.navarra.es>
2025-12-19 13:48:36 +0100

    Ejercicio 5

--- src/ex06_emphasize_vowel.py
+++ src/ex06_emphasize_vowel.py
@@ -18,20 +18,5 @@ def emphasize_vowel(phrase: str, vowel: str) -> str:
     - Recorre la frase carácter a carácter y construye una nueva cadena.
     """
     # TODO: validar y transformar
-    def emphasize_vowel(frase: str, vocal: str) -> str:
-    if len(vocal) != 1:
-        raise ValueError("Debe ser un solo carácter")
     
-    vocal_min = vocal.lower()
-    if vocal_min not in "aeiou":
-        raise ValueError("La vocal debe ser a, e, i, o, u")
-    
-    resultado = ""
-    for caracter in frase:
-        if caracter.lower() == vocal_min:
-            resultado += caracter.upper()
-        else:
-            resultado += caracter
-    
-    return resultado
     raise NotImplementedError("Implementa emphasize_vowel(phrase, vowel)")
